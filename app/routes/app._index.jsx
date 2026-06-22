4076c028195de2dd592575ab070ecb38e763f002
Shrikant Korde <shrikantkorde93@gmail.com>
2026-06-22 16:58:57 +0530

    Initial Shopify App

---
+++ app/routes/app._index.jsx
@@ -0,0 +1,139 @@
+import { useEffect } from "react";
+import { useFetcher } from "react-router";
+import { useAppBridge } from "@shopify/app-bridge-react";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+
+  return null;
+};
+
+export const action = async ({ request }) => {
+  const { admin } = await authenticate.admin(request);
+  const color = ["Red", "Orange", "Yellow", "Green"][
+    Math.floor(Math.random() * 4)
+  ];
+  const response = await admin.graphql(
+    `#graphql
+      mutation populateProduct($product: ProductCreateInput!) {
+        productCreate(product: $product) {
+          product {
+            id
+            title
+            handle
+            status
+            variants(first: 10) {
+              edges {
+                node {
+                  id
+                  price
+                  barcode
+                  createdAt
+                }
+              }
+            }
+            demoInfo: metafield(namespace: "$app", key: "demo_info") {
+              jsonValue
+            }
+          }
+        }
+      }`,
+    {
+      variables: {
+        product: {
+          title: `${color} Snowboard`,
+          metafields: [
+            {
+              namespace: "$app",
+              key: "demo_info",
+              value: "Created by React Router Template",
+            },
+          ],
+        },
+      },
+    },
+  );
+  const responseJson = await response.json();
+  const product = responseJson.data.productCreate.product;
+  const variantId = product.variants.edges[0].node.id;
+  const variantResponse = await admin.graphql(
+    `#graphql
+    mutation shopifyReactRouterTemplateUpdateVariant($productId: ID!, $variants: [ProductVariantsBulkInput!]!) {
+      productVariantsBulkUpdate(productId: $productId, variants: $variants) {
+        productVariants {
+          id
+          price
+          barcode
+          createdAt
+        }
+      }
+    }`,
+    {
+      variables: {
+        productId: product.id,
+        variants: [{ id: variantId, price: "100.00" }],
+      },
+    },
+  );
+  const variantResponseJson = await variantResponse.json();
+  const metaobjectResponse = await admin.graphql(
+    `#graphql
+    mutation shopifyReactRouterTemplateUpsertMetaobject($handle: MetaobjectHandleInput!, $metaobject: MetaobjectUpsertInput!) {
+      metaobjectUpsert(handle: $handle, metaobject: $metaobject) {
+        metaobject {
+          id
+          handle
+          title: field(key: "title") {
+            jsonValue
+          }
+          description: field(key: "description") {
+            jsonValue
+          }
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }`,
+    {
+      variables: {
+        handle: {
+          type: "$app:example",
+          handle: "demo-entry",
+        },
+        metaobject: {
+          fields: [
+            { key: "title", value: "Demo Entry" },
+            {
+              key: "description",
+              value:
+                "This metaobject was created by the Shopify app template to demonstrate the metaobject API.",
+            },
+          ],
+        },
+      },
+    },
+  );
+  const metaobjectResponseJson = await metaobjectResponse.json();
+
+  return {
+    product: responseJson.data.productCreate.product,
+    variant: variantResponseJson.data.productVariantsBulkUpdate.productVariants,
+    metaobject: metaobjectResponseJson.data.metaobjectUpsert.metaobject,
+  };
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Bundle Upsell App</h1>
+    </div>
+  );
+}
+
+export const headers = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
